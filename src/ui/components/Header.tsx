02247e82a1b1a08cfdd01e2a068102a81858dd20
root <root@gitea.friendlysquad.com>
2025-08-24 21:14:17 +0200

    Initial commit

---
+++ src/ui/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Logo } from "./Logo";
+import { CartNavItem } from "./nav/components/CartNavItem";
+
+export function Header({ channel }: { channel: string }) {
+	return (
+		<header className="sticky top-0 z-20 bg-neutral-100/50 backdrop-blur-md">
+			<div className="mx-auto max-w-7xl px-3 sm:px-8">
+				<div className="flex h-16 items-center justify-between gap-4 md:gap-8">
+					<Logo />
+					<div className="flex items-center">
+						<CartNavItem channel={channel} />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+}
